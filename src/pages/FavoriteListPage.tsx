50d4ef472f2aa5af672c80f0ea692c8e1687920f
Ivan Kazantsev <io.kazantcev@ya.ru>
2023-05-12 13:07:52 +0300

    deleted unused vars

--- src/pages/FavoriteListPage.tsx
+++ src/pages/FavoriteListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { ContactCard } from "src/components/ContactCard";
 import { ContactDto } from "src/types/dto/ContactDto";
